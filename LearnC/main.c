297500d488bf00b77d3f4df35feaa73881163eb6
mojun <immojun@gmail.com>
2016-03-08 13:05:19 +0800

    init

---
+++ LearnC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LearnC
+//
+//  Created by mojun on 16/3/8.
+//  Copyright © 2016年 kimoworks. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
